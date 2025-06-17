f4cb58b3a0a9421bac71a1aa4e786dec91c788ad
noslipper <380886011@qq.com>
2025-06-17 09:39:32 +0800

    Update fab.js

--- fab.js
+++ fab.js
@@ -2,7 +2,13 @@
 // @name        FAB Free Asset Getter Latest
 // @namespace   Violentmonkey Scripts
 // @copyright   https://greasyfork.org/zh-CN/users/313682-没拖鞋 | 2024, subtixx (https://openuserjs.org/users/subtixx)
-// @match       https://www.fab.com/*
+// @match       https://www.fab.com/channels/*
+// @match       https://www.fab.com/sellers/*
+// @match       https://www.fab.com/zh-cn/channels/*
+// @match       https://www.fab.com/zh-cn/sellers/*
+// @match       https://www.fab.com/limited-time-free
+// @match       https://www.fab.com/search?*
+// @match       https://www.fab.com/zh-cn/search?*
 // @grant       none
 // @license     AGPL-3.0-or-later
 // @version     2.1.2
